c0df40ab805478883c398c4c948ae872fb3c2bb7
alex <alexdragoak@gmail.com>
2020-05-20 16:34:22 +0200

    Busqueda hecha

--- src/Entity/MangaList.php
+++ src/Entity/MangaList.php
@@ -2,13 +2,26 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiFilter;
 use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 use App\Repository\MangaListRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use ApiPlatform\Core\Serializer\Filter\PropertyFilter;
+use Symfony\Component\Serializer\Annotation\SerializedName;
 
 /**
- * @ApiResource()
+ * @ApiResource(
+ *     normalizationContext={"groups"={"manga_listado:read"},"swagger_definition_name"="Lectura"},
+ *     denormalizationContext={"groups"={"manga_listado:write"},"swagger_definition_name"="Escritura"},
+ *     shortName="Mangas"
+ * )
  * @ORM\Entity(repositoryClass=MangaListRepository::class)
+ * @ApiFilter(
+ *     SearchFilter::class,
+ *     properties={"Nombre":"partial",
+ *     PropertyFilter::class})
  */
 class MangaList
 {
@@ -16,26 +29,35 @@ class MangaList
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     * @Groups({"manga_listado:read"})
      */
     private $id;
 
     /**
+     * Nombre del manga
      * @ORM\Column(type="string", length=255)
+     * @Groups({"manga_listado:read","manga_listado:write"})
      */
     private $Nombre;
 
     /**
+     * Autor del manga
      * @ORM\Column(type="string", length=50, nullable=true)
+     * @Groups({"manga_listado:read","manga_listado:write"})
      */
     private $Autor;
 
     /**
+     * Descripcion del manga
+     * @Groups({"manga_listado:read"})
      * @ORM\Column(type="string", length=400, nullable=true)
      */
     private $Descripcion;
 
     /**
+     * Imagen de portada del manga
      * @ORM\Column(type="string", length=50, nullable=true)
+     * @Groups({"manga_listado:read","manga_listado:write"})
      */
     private $Portada;
 
@@ -73,11 +95,37 @@ class MangaList
         return $this->Descripcion;
     }
 
+
     public function setDescripcion(?string $Descripcion): self
     {
         $this->Descripcion = $Descripcion;
 
         return $this;
+    }
+
+    /**
+     * Descripcion del manga.
+     *
+     * @Groups({"manga_listado:write"})
+     * @SerializedName("Descripcion")
+     */
+    public function setTextDescripcion(?string $Descripcion): self
+    {
+        $this->Descripcion = nl2br($Descripcion);
+
+        return $this;
+    }
+
+    /**
+     * Descripcion corta
+     * @Groups({"manga_listado:write"})
+     */
+    public function getDescriptionCorta(): string
+    {
+        if (strlen($this->Descripcion) < 40) {
+            return $this->Descripcion;
+        }
+        return substr($this->Descripcion, 0, 40).'...';
     }
 
     public function getPortada(): ?string
